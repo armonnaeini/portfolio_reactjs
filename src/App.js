b211c387970f2b8b9430df63a56a5c8849d44758
Armon Naeini <armonnaeini@Armons-MBP.attlocal.net>
2020-10-07 11:30:16 -0500

    added link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import About from './Components/About';
 import Work from './Components/Work';
 import Contact from './Components/Contact';
+
 
 
 function App() {
